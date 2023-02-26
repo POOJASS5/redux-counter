ade9ebeaf24ed86b9075c52715510b66fa639a75
POOJA <121348592+poojaANUP@users.noreply.github.com>
2023-02-26 16:31:24 +0530

    subscribing to redux store and using the dispatch function

--- Counter.js
+++ Counter.js
@@ -1,10 +1,8 @@
 import { useSelector, useDispatch } from "react-redux";
 import classes from "./Counter.module.css";
-
 const Counter = () => {
   const dispatch = useDispatch();
   const counter = useSelector((state) => state.counter);
-
   const incrementHandler = () => {
     dispatch({ type: "increment" });
   };
@@ -13,11 +11,11 @@ const Counter = () => {
   };
 
   const incrementHandler2 = () => {
-    dispatch({ type: "incrementBy2" });
+    dispatch({ type: "incrementBy5" });
   };
 
   const decrementHandler2 = () => {
-    dispatch({ type: "decrementBy2" });
+    dispatch({ type: "decrementBy5" });
   };
   const toggleCounterHandler = () => {};
 
@@ -28,12 +26,11 @@ const Counter = () => {
       <div>
         <button onClick={incrementHandler}>Increment</button>
         <button onClick={deccrementHandler}>Decrement</button>
-        <button onClick={incrementHandler2}>Increment by 2</button>
-        <button onClick={decrementHandler2}>Decrement by 2</button>
+        <button onClick={incrementHandler2}>Increment by 5</button>
+        <button onClick={decrementHandler2}>Decrement by 5</button>
       </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
 };
-
 export default Counter;
